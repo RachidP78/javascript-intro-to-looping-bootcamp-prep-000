e0788f08e0277deef08bf7c0f9f7d5f376d5cdbe
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-24 07:54:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,11 +1,14 @@
 // forLoop(array)
 
 function forLoop(array) {
-  for (let i = 1; i <= 25; i++) {
-    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
   }
-  return array
-
+  return array;
 }
 
 // whileLoop(n)
